a767455d940786881d621f205a0247a655a565fd
mercuricchloride <mercuryprotocol@protonmail.com>
2023-03-09 19:35:45 -0800

    test

--- Components/EditorInterface.tsx
+++ Components/EditorInterface.tsx
@@ -25,6 +25,7 @@ export default function EditorInterface() {
       },
     });
     const Solidity = await Parser.Language.load("tree-sitter-solidity.wasm");
+    console.log("Solidity", Solidity);
 
     const parser = new Parser();
     console.log("parser", parser);
