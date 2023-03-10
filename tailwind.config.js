5099234bd44b1aaf6c6d8a2e98540c6c0feb885a
mercuricchloride <mercuryprotocol@protonmail.com>
2023-03-09 19:49:00 -0800

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-} 
+};
